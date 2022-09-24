cc588200a69bf67fbe1fa61eaaf274e9c0f4141b
otsabi <sabi.othmane@gmail.com>
2022-09-24 16:53:37 +0100

    test5

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,6 @@
 <template>
   <b-container fluid>
-    test4
+    test5
     <b-row>
       <b-col offset="4" md="4">
         <v-select v-model="categoryFilter" :options="categorys" class="w-100" />
